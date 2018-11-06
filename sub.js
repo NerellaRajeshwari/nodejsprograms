fb8dcb36769b3384476dc294f19dc8a8d7ddb1cc
NerellaRajeshwari <36570159+NerellaRajeshwari@users.noreply.github.com>
2018-11-06 21:03:27 +0530

    Create sub.js

---
+++ sub.js
@@ -0,0 +1,9 @@
+code:
+var n = Number(process.argv[2]);
+var m = Number(process.argv[3]);
+var result=n - m;
+console.log('sub is '+result);
+
+output:
+C:\Users\Rajeshwary\Desktop\javascripting\my-dream-app>node index.js 10 5
+sub is 5
