d9a128e915f799e058c900894e4a3a3c07a131dd
NerellaRajeshwari <36570159+NerellaRajeshwari@users.noreply.github.com>
2018-11-06 21:05:34 +0530

    Update mul.js

--- mul.js
+++ mul.js
@@ -2,7 +2,7 @@ code:
 var n = Number(process.argv[2]);
 var m = Number(process.argv[3]);
 var result=n * m;
-console.log('sub is '+result);
+console.log('mul is '+result);
 
 output:
 C:\Users\Rajeshwary\Desktop\javascripting\my-dream-app>node index.js 10 5
