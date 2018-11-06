ea6b2dee32207c06f8455deb387988cb3cfa8708
NerellaRajeshwari <36570159+NerellaRajeshwari@users.noreply.github.com>
2018-11-06 21:01:03 +0530

    Create add.js

---
+++ add.js
@@ -0,0 +1,9 @@
+code:
+var n = Number(process.argv[2]);
+var m = Number(process.argv[3]);
+var result=n + m;
+console.log('sum is '+result);
+
+output:
+C:\Users\Rajeshwary\Desktop\javascripting\my-dream-app>node index.js 1 2
+sum is 3
